6e18c9bb8b7c27a97bcf230798f47605491bcf4b
Giovanny Vargas <yovaz05@gmail.com>
2022-01-26 18:56:12 -0500

    add button"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./Pages/Home";
+import Infantil from "./Pages/Infantil";
+import Peliculas from "./Pages/Peliculas";
+import Series from "./Pages/Series";
+import Signup from "./Pages/Signup";
+import Tv from "./Pages/Tv";
 
 function App() {
-  return <div className="App">Example</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/tv" element={<Tv />} />
+          <Route path="/series" element={<Series />} />
+          <Route path="/peliculas" element={<Peliculas />} />
+          <Route path="/infantil" element={<Infantil />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Pages/Infantil.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Infantil = () => {
+  return <div>Infantil</div>;
+};
+
+export default Infantil;
